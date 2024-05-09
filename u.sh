2d0698c08ddd61693fc8837d95200796e6a6b6c1
OliveIsAWord <89336634+OliveIsAWord@users.noreply.github.com>
2024-05-09 11:37:50 -0600

    cat-erpillar

--- u.sh
+++ u.sh
@@ -1,2 +1,3 @@
 meow="steam-run ~/Documents/microw8/uw8"
-$meow pack -l 2 main.cwa main.uw8 && $meow run main.uw8
+#$meow pack -l 2 main.cwa main.uw8 && $meow run main.uw8
+$meow run --filter nearest main.cwa
